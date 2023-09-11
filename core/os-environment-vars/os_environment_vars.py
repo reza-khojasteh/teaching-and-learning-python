406614ed36a0b088fe3c89e423a8ffd8ff4766da
reza-khojasteh <mohammad.reza.khojasteh@gmail.com>
2023-09-11 18:46:42 -0400

    Modify 'os_environment_vars.py'

--- core/os-environment-vars/os_environment_vars.py
+++ core/os-environment-vars/os_environment_vars.py
@@ -1,24 +1,7 @@
 """
 Checks the ENV_NAME environment variable to determine the environment.
-
-Parameters:
-DEVELOPMENT (str): The string representing the development environment.
-STAGING (str): The string representing the staging environment. 
-PRODUCTION (str): The string representing the production environment.
-
-current_env (str): Gets the ENV_NAME environment variable, defaulting to DEVELOPMENT.
-
-if current_env == DEVELOPMENT:
-Prints a message indicating the development environment.
-
-elif current_env == PRODUCTION:  
-Prints a message indicating the production environment.
-
-elif current_env == STAGING:
-Prints a message indicating the staging environment.
-
-else:  
-Prints a message indicating an unknown environment.
+current_env (a str) gets the ENV_NAME environment variable, defaulting to DEVELOPMENT.
+You can test this program using "export ENV_NAME=something_else" in terminal (Mac/Linux.)
 """
 
 import os
@@ -35,6 +18,5 @@ elif current_env == PRODUCTION:
     print("Production environment")
 elif current_env == STAGING:
     print("Staging environment")
-# Can test using "export ENV_NAME=something_else" in terminal (Mac/Linux)
 else:
     print("Unknown environment")
