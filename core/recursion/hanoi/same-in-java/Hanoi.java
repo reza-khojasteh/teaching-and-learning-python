6b3e0691ee8d5b78ed9cb6391a6e91d98fbc4b45
Reza Khojasteh <reza-khojasteh@users.noreply.github.com>
2023-10-16 20:33:59 +0000

    Modify 'Hanoi.java'

--- core/recursion/hanoi/same-in-java/Hanoi.java
+++ core/recursion/hanoi/same-in-java/Hanoi.java
@@ -1,21 +1,41 @@
-// hanoi towers in java
+
+// Recursive solution to the Towers of Hanoi problem.
 import java.util.Scanner;
 
 public class Hanoi {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number of disks: ");
         int n = sc.nextInt();
-        hanoi(n, 'A', 'B', 'C');
+        hanoi('f', 's', 't', n);
         sc.close();
     }
 
-    public static void hanoi(int n, char from, char to, char aux) {
-        if (n == 1) {
-            System.out.println("Move disk 1 from rod " + from + " to rod " + to);
+    // Parameters:
+    // f (str): Starting rod
+    // s (str): Spare rod
+    // t (str): Ending rod
+    // n (int): Number of disks
+
+    // Functionality:
+    // Moves n disks from rod f to rod t using rod s.
+    // If n <= 0, returns.
+    // If n == 1, prints the move from f to t.
+    // Otherwise:
+    // - Moves n-1 disks from f to s using t.
+    // - Prints the move from f to t.
+    // - Moves n-1 disks from s to t using f.
+    public static void hanoi(char f, char s, char t, int n) {
+        // Base case(s)
+        if (n <= 0)
             return;
+        if (n == 1)
+            System.out.println(f + " -> " + t);
+        // recursive case:
+        else {
+            hanoi(f, t, s, n - 1);
+            System.out.println(f + " -> " + t);
+            hanoi(s, f, t, n - 1);
         }
-        hanoi(n - 1, from, aux, to);
-        System.out.println("Move disk " + n + " from rod " + from + " to rod " + to);
-        hanoi(n - 1, aux, to, from);
     }
 }
