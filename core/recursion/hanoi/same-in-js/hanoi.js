e4d57a54f4b063959502c92f83b0633b45748b4f
Reza Khojasteh <reza-khojasteh@users.noreply.github.com>
2023-10-21 02:48:34 +0000

    Modify hanoi.js

--- core/recursion/hanoi/same-in-js/hanoi.js
+++ core/recursion/hanoi/same-in-js/hanoi.js
@@ -1,10 +1,21 @@
 // hanoi towers in js
 function hanoi(f, s, t, n) {
-    if (n > 0) {
-        hanoi(f, t, s, n - 1);
-        console.log(`${f} -> ${t}`);
-        hanoi(s, f, t, n - 1);
+    // if (n > 0) {
+    //     hanoi(f, t, s, n - 1);
+    //     console.log(`${f} -> ${t}`);
+    //     hanoi(s, f, t, n - 1);
+    // }
+    // OR
+    if (n <= 0) {
+        return;
     }
+    if (n === 1) {
+        console.log(`${f} -> ${t}`);
+        return;
+    }
+    hanoi(f, t, s, n - 1);
+    console.log(`${f} -> ${t}`);
+    hanoi(s, f, t, n - 1);
 }
 
 hanoi('f', 's', 't', 3);
